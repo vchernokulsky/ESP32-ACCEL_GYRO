12b1ec408fa4749b88062b09938e1e104aa1a3fd
yana <yanchik_vau@mail.ru>
2020-07-06 11:25:29 +0300

    init AccelServer

---
+++ software/AccelServer/AccelServer/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	private AccelServer.SocketServer socket;
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	
+		socket = new AccelServer.SocketServer("Hello world!!!");
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton4Clicked (object sender, EventArgs e)
+	{
+		socket.PrintStr (10);
+	}
+}
---
+++ software/AccelServer/AccelServer/Scripts/SocketServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AccelServer
+{
+	public class SocketServer
+	{
+		private String str;
+		public SocketServer (String s)
+		{
+			str = s;
+		}
+		public void PrintStr(int t)
+		{
+			for (int i = 0; i < t; i++) 
+			{
+				Console.WriteLine (String.Concat(i.ToString(), str));
+			}
+		}
+	}
+}
+
