8331a89914abfea821729784275f8005cafd61be
yana <yanchik_vau@mail.ru>
2020-07-07 11:11:40 +0300

    broadcast server ip

--- software/AccelServer/AccelServer/MainWindow.cs
+++ software/AccelServer/AccelServer/MainWindow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Gtk;
 using AccelServer;
+using System.Threading;
 
 public partial class MainWindow: Gtk.Window
 {
@@ -8,6 +9,11 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+
+		int port = 9876;
+		ConnectionController controller = new ConnectionController (port);
+		Thread ipBroadcaster = new Thread(new ThreadStart(controller.IpBroadcast));
+		ipBroadcaster.Start();
 	
 	}
 
@@ -19,7 +25,6 @@ public partial class MainWindow: Gtk.Window
 
 	protected void OnButton4Clicked (object sender, EventArgs e)
 	{
-		ConnectionController c = new ConnectionController ();
-		c.SendIp ();
+		
 	}
 }
--- software/AccelServer/AccelServer/Scripts/ConnectionController.cs
+++ software/AccelServer/AccelServer/Scripts/ConnectionController.cs
@@ -2,23 +2,31 @@
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Threading;
 
 namespace AccelServer
 {
 	public class ConnectionController
 	{
-		public ConnectionController ()
+		private int port;
+		public ConnectionController (int port)
 		{
+			this.port = port;
 		}
 
-		public void SendIp()
+		public void IpBroadcast()
 		{
-
-			int PORT = 9876;
 			UdpClient udpClient = new UdpClient();
-			udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, PORT));
-			var data = Encoding.UTF8.GetBytes("ABCD");
-			udpClient.Send(data, data.Length, "255.255.255.255", PORT); 
+			udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+			var host = Dns.GetHostEntry(Dns.GetHostName());
+			String addres = (host.AddressList [0]).ToString();
+			var data = Encoding.UTF8.GetBytes(addres);
+			while(true)
+			{
+				udpClient.Send(data, data.Length, "255.255.255.255", port);
+				Thread.Sleep(3000);
+			}
+
 		}
 	}
 }
