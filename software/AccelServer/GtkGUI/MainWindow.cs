a03b8a757206276212ca0b34cdc6d318b2b25847
yana <yanchik_vau@mail.ru>
2020-07-14 15:50:45 +0300

    add GtkGui for test

---
+++ software/AccelServer/GtkGUI/MainWindow.cs
@@ -0,0 +1,31 @@
+﻿using AccelServer;
+using System;
+using Gtk;
+
+
+
+public partial class MainWindow: Gtk.Window
+{
+	private AccelServer.AccelServer Accel;
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		Accel = new AccelServer.AccelServer (15000);
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton4Clicked (object sender, EventArgs e)
+	{
+		Accel.StartReceiving ();
+	}
+
+	protected void OnButton5Clicked (object sender, EventArgs e)
+	{
+		Accel.StopReceiving ();
+	}
+}
